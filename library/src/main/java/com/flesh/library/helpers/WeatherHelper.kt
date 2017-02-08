29889bcbbcd751b9863a32e0a2cc3376f514a735
CAPS\afleshner <aaron.fleshner@ua.edu>
2017-02-08 12:34:54 -0600

    Moved the packages Around in the library

--- library/src/main/java/com/flesh/library/helpers/WeatherHelper.kt
+++
@@ -1,6 +0,0 @@
-package com.flesh.weatherforcast.helper
-
-/**
- * Created by afleshner on 2/3/2017.
- */
-class WeatherHelper (var startPeriodName : String,var tempLabel : String,var temperature : String?,var weather : String,var pop:String?,var iconLink:String)
