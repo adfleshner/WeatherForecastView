1db9fa57232b82aaef02037242a2d2f59ae475e3
CAPS\afleshner <aaron.fleshner@ua.edu>
2017-02-08 14:16:44 -0600

    Lib upload things

--- library/src/main/kotlin/com/flesh/weatherforecastview/library/helpers/WeatherHelper.kt
+++ library/src/main/kotlin/com/flesh/weatherforecastview/library/helpers/WeatherHelper.kt
@@ -2,6 +2,6 @@ package com.flesh.weatherforecastview.library.helpers
 
 /**
  * Created by afleshner on 2/3/2017.
- * The Weatherforecast object that comes back splits all of these variable into their own list this object combines them for ease.
+ * The Weather Forecast object that comes back splits all of these variable into their own list this object combines them for ease.
  */
 class WeatherHelper (var startPeriodName : String,var tempLabel : String,var temperature : String?,var weather : String,var pop:String?,var iconLink:String)
